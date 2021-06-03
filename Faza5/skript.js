adbe8cc153321af167f04fc22d098fbc3e63c1c4
zoranacerovac <79044601+zoranacerovac@users.noreply.github.com>
2021-06-03 21:32:32 +0200

    Add files via upload

---
+++ Faza5/skript.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+
+//     function postaviTabeluNovost(){
+//         red = $("<tr></tr>");
+//         for(let i = 0; i++; i<3){
+//             let novi_td = $("<td></td>");
+            
+//             red.append(novi_td);
+//         }
+//         $("novosti-tabela").append(red);
+//     }
+// });
+
